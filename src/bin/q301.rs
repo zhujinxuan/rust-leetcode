774cd5b9e7e57dd12f8d39cd35a28c6bbfde9e2e
zhujinxuan <zhujinxuan@gmail.com>
2020-07-25 17:19:32 -0400

    Bootstrap

---
+++ src/bin/q301.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn remove_invalid_parentheses(s: String) -> Vec<String> {
+        unimplemented!(s);
+    }
+}
+
+
+fn main () {
+    assert_eq!(Vec::<String>::new(), Solution::remove_invalid_parentheses(String::from(")(")));
+}
+
+struct Solution;
